c48ed3d7d068958c75cb4dc4821f8ef1f68f71b5
Raul Quispe <relaxedchild@gmail.com>
2021-08-06 18:11:03 -0500

    apply format and small tweaks

--- session-03/exercises/transformation-iteration/solutions/transformationIteration.imperative.js
+++ session-03/exercises/transformation-iteration/solutions/transformationIteration.imperative.js
@@ -1,11 +1,11 @@
 function transform(source, target) {
-    const result = [source]
+  const result = [source];
 
-    for (let i = 0; i < source.length; i++) {
-        if (source[i] !== target[i]) {
-            result.push(target.slice(0, i + 1) + source.slice(i + 1))
-        }
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] !== target[i]) {
+      result.push(target.slice(0, i + 1) + source.slice(i + 1));
     }
+  }
 
-    return result
+  return result;
 }
