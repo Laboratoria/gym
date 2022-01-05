78da7946b5709aaa77a3473bcccf272441b2270d
merunga <merunga@gmail.com>
2022-01-05 00:13:26 -0500

    layout

--- gym-search/components/more-stories.js
+++ gym-search/components/more-stories.js
@@ -3,7 +3,7 @@ import PostPreview from '../components/post-preview'
 export default function MoreStories({ posts }) {
   return (
     <section>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-10 mb-26">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
--- gym-search/components/post-preview.js
+++ gym-search/components/post-preview.js
@@ -13,15 +13,17 @@ export default function PostPreview({
 }) {
   return (
     <div>
-      <div className="mb-5">
-        <CoverImage
-          slug={slug}
-          title={title}
-          src={coverImage}
-          height={278}
-          width={556}
-        />
-      </div>
+      {coverImage && (
+        <div className="mb-5">
+          <CoverImage
+            slug={slug}
+            title={title}
+            src={coverImage}
+            height={278}
+            width={556}
+          />
+        </div>
+      )}
       <h3 className="text-3xl mb-3 leading-snug">
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a className="hover:underline" dangerouslySetInnerHTML={{__html: title}} />
