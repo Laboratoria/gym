567a1f2f2f77df96252f3a96a36139f050045226
merunga <merunga@gmail.com>
2022-01-05 00:18:12 -0500

    quitar import css problematico

--- gym-search/components/post-body.js
+++ gym-search/components/post-body.js
@@ -1,10 +1,9 @@
-import markdownStyles from './markdown-styles.module.css'
+// import markdownStyles from './markdown-styles.module.css'
 
 export default function PostBody({ content }) {
   return (
     <div className="max-w-2xl mx-auto">
       <div
-        className={markdownStyles['markdown']}
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
