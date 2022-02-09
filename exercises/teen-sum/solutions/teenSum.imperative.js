83d16c1ee7d2220e3058085baa9ea7a0b49b7264
Sergio Sinuco <ssinuco@gmail.com>
2022-02-09 11:57:21 -0500

    Apply suggestions from @merunga and @santiaguf code review

--- exercises/teen-sum/solutions/teenSum.imperative.js
+++ exercises/teen-sum/solutions/teenSum.imperative.js
@@ -1,12 +1,6 @@
 function teenSum(a, b) {
-  return (
-    (
-      (
-        (a >= 13 && a <= 19)
-        || (b >= 13 && b <= 19)
-      )
-      && 19
-    )
-    || a + b
-  );
+  if ((a >= 13 && a <= 19) || (b >= 13 && b <= 19)) {
+    return 19;
+  }
+  return a + b;
 }
