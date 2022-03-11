07fedf25cc457568d0c21d94431ae98416952220
Sebastian Silva <sebastian@fuentelibre.org>
2022-03-10 23:49:11 -0500

    Fix eslint errors

--- exercises/format-words/solutions/formatWordsReduce.declarative.js
+++ exercises/format-words/solutions/formatWordsReduce.declarative.js
@@ -1,12 +1,8 @@
-function formatWords(words){
-    return (words || [])
-    .filter( w => w)
+function formatWords(words) {
+  return (words || [])
+    .filter((w) => w)
     .reduce(
-    (str, word, index, array) => {
-    return index === 0
-    ? word
-    : index === array.length -1
-    ? `${str} and ${word}`
-    : `${str}, ${word}`
-    }, '')
+      (str, word, index, array) => (index === 0 ? word : index === array.length - 1 ? `${str} and ${word}` : `${str}, ${word}`), // eslint-disable-line no-nested-ternary
+      '',
+    );
 }
--- exercises/star-lovers-warmup/solutions/stars.declarative.js
+++ exercises/star-lovers-warmup/solutions/stars.declarative.js
@@ -1,1 +1,1 @@
-const printStars = (number) => number >= 0 ? "*".repeat(number) : "invalid parameter";
+const printStars = (number) => (number >= 0 ? '*'.repeat(number) : 'invalid parameter');
