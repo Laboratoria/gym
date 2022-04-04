243cf4f90f2695d295700a632408d98d5ac92c01
Santiago Bernal <exactlimon@gmail.com>
2022-04-04 12:01:49 -0500

    [update] exercises with eslint recommendations

--- exercises/is-this-a-triangle/solutions/isTriangle.imperative.js
+++ exercises/is-this-a-triangle/solutions/isTriangle.imperative.js
@@ -1,9 +1,11 @@
-function isTriangle(a,b,c) {
-  if(a > 0 && b > 0 && c > 0) {
-    if(a < b + c && b < a + c && c < a + b) {
+function isTriangle(a, b, c) {
+  if (a > 0 && b > 0 && c > 0) {
+    if (a < b + c && b < a + c && c < a + b)
+    {
       return true;
     }
-    else {
+    else
+    {
       return false;
     }
   }
