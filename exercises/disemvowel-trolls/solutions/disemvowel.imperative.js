740196c93f2c3eb137182512064538df5dafe3b5
adolivaresl <domioli@gmail.com>
2022-04-04 16:10:37 -0400

    exercise and correction README for session 12

--- exercises/disemvowel-trolls/solutions/disemvowel.imperative.js
+++ exercises/disemvowel-trolls/solutions/disemvowel.imperative.js
@@ -1,7 +1,6 @@
 function disemvowel(str) {
   const vowels = 'aeiou';
   let newStr = '';
-
   for (let index = 0; index < str.length; index++) {
     if (!vowels.includes(str[index].toLowerCase())) {
       newStr += str[index];
