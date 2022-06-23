37051721d9ce03487a45e07b02771d792fa1c14a
Sebastian Silva <sebastian@fuentelibre.org>
2022-06-23 03:24:29 -0500

    Add better markdown rendering

--- gym-search/components/post-body.js
+++ gym-search/components/post-body.js
@@ -2,7 +2,7 @@
 
 export default function PostBody({ content }) {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-2xl mx-auto prose">
       <div
         dangerouslySetInnerHTML={{ __html: content }}
       />
--- gym-search/tailwind.config.js
+++ gym-search/tailwind.config.js
@@ -30,4 +30,7 @@ module.exports = {
       },
     },
   },
+  plugins: [
+    require("@tailwindcss/typography")
+  ]
 }
